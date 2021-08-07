990ccf05a0b66c2f6a1346e6fd1d0cb39103d47d
Jason Washburn <wburn@wburn.net>
2021-08-07 05:59:01 -0500

    added basic production chart function

--- vtcalc.py
+++ vtcalc.py
@@ -48,6 +48,35 @@ def parse_model_run_from_string(model_run_str: str) -> dt.datetime:
     return model_run
 
 
+def production_hours_chart(model_run_str: str) -> None:
+    """Generates a chart of production hours for the last 8 model runs"""
+    model_run = parse_model_run_from_string(model_run_str)
+    valid_time = get_valid_time_from_offset(model_run, 0)
+    valid_time_str = valid_time.strftime("%Y%m%d%H")
+    print("Valid time: {}".format(valid_time_str))
+    print("")
+
+    # Get the last 8 model runs
+    model_runs = [model_run - dt.timedelta(hours=i * 6) for i in range(8)]
+
+    # Print the model run times
+    print("Model Run\t\t+36\t\t+39\t\t+42\n")
+    for model_run in model_runs:
+        production_hours = []
+        for production_hour in [36, 39, 42]:
+            production_hours.append(
+                (model_run + dt.timedelta(hours=production_hour)).strftime(
+                    "%Y-%m-%d %Hz"
+                )
+            )
+        print(
+            model_run.strftime("%Y-%m-%d %Hz")
+            + ": \t"
+            + "\t ".join(production_hours)
+            + "\n"
+        )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Calculate a valid time from a model run and offset."
